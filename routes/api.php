9a54f9daa3347845fb6b948a3a589434e4b16752
Dev.Alysonh <dev.alysonh@gmail.com>
2024-07-22 15:29:27 -0300

    Implements Auth with JWT token

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+	Route::post('login', [AuthController::class, 'login']);
+	Route::post('register', [AuthController::class, 'register']);
+});
+
+Route::middleware('auth:api')->group(function () {
+	Route::post('logout', [AuthController::class, 'logout']);
+	Route::post('refresh', [AuthController::class, 'refresh']);
+	Route::get('me', [AuthController::class, 'me']);
+});

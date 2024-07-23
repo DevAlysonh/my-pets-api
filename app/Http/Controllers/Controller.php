bd4756856b82520be609d7cbcde5bd1e0f63b2e0
Dev.Alysonh <dev.alysonh@gmail.com>
2024-07-22 21:21:58 -0300

    Implements swagger on the authentication routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(title="My Pets Api", version="0.0.1")
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ * )
+ */
 abstract class Controller
 {
     //
